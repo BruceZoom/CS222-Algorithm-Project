7cca24d319ae69d7ddf5523731691d26e71f473d
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-12-26 00:03:47 +0800

    include layer2idx mapping

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,44 @@ import os
 import json
 import numpy as np
 
+# layer names are sorted
+layer2idx = {
+    'Conv1': (0, 64),
+    'Conv10': (64, 576),
+    'Conv11': (576, 1088),
+    'Conv12': (1088, 1600),
+    'Conv13': (1600, 2112),
+    'Conv2': (2112, 2176),
+    'Conv3': (2176, 2304),
+    'Conv4': (2304, 2432),
+    'Conv5': (2432, 2688),
+    'Conv6': (2688, 2944),
+    'Conv7': (2944, 3200),
+    'Conv8': (3200, 3712),
+    'Conv9': (3712, 4224),
+    'FC14': (4224, 8320),
+    'FC15': (8320, 12416)
+}
+
+
+# layer2idx = {
+#     'Conv1': (0, 64),
+#     'Conv10': (2176, 2688),
+#     'Conv11': (2688, 3200),
+#     'Conv12': (3200, 3712),
+#     'Conv13': (3712, 4224),
+#     'Conv2': (64, 128),
+#     'Conv3': (128, 256),
+#     'Conv4': (256, 384),
+#     'Conv5': (384, 640),
+#     'Conv6': (640, 896),
+#     'Conv7': (896, 1152),
+#     'Conv8': (1152, 1664),
+#     'Conv9': (1664, 2176),
+#     'FC14': (4224, 8320),
+#     'FC15': (8320, 12416),
+# }
+
 
 def load_data(root="CriticalPathPruning/ImageEncoding", classes=None):
     if classes is None:
@@ -32,8 +70,20 @@ def to_txt(path, data, labels):
     np.savetxt(os.path.join(path, "labels.txt"), labels)
 
 
+def update_layer2idx():
+    dist = 0
+    for key, item in layer2idx.items():
+        incr = item[1] - item[0]
+        layer2idx[key] = (dist, dist + incr)
+        dist += incr
+    print(layer2idx)
+
+
 if __name__ == '__main__':
-    data, labels, sample_names = load_data(classes=['class0', 'class1', 'class2', 'class3', 'class4',])
+    data, labels, sample_names = load_data(classes=['class0', 'class1', 'class2', 'class3', 'class4', ])
     print(data.shape, labels.shape)
-    print(np.histogram(labels, bins=15))
+    print(np.histogram(labels, bins=np.unique(labels).shape[0]))
     to_txt("./data", data, labels)
+
+    # update_layer2idx()
+    pass
